7ea359f1e7200559167a2914ba56c40a46861ab5
yuji4539 <yuji4539@gmail.com>
2020-12-12 17:03:10 +0900

    누락된node_modules 포함하여 업로드 / 타이머 테스트 코드 테스트

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet, View, TouchableOpacity, Text,
+} from 'react-native';
+import { Timer, FlipNumber } from 'react-native-flip-timer';
+
+export default class App extends Component {
+  state = {
+    play: true,
+  }
+
+  play = () => {
+    this.setState(({ play }) => ({ play: !play }));
+  }
+
+  render() {
+    const { play } = this.state;
+    return (
+      <View style={styles.container}>
+        <Timer time={0} play={play} />
+        <TouchableOpacity style={styles.button} onPress={this.play}>
+          <Text style={styles.text}>{play ? 'Pause' : 'Play'}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    height: 40,
+    backgroundColor: '#333333',
+    width: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#cccccc',
+  },
+});
